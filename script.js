d265c4e0c81d795e72355fb881bbd34ec65f277e
kee_hoon <djfkfk12345@naver.com>
2022-11-09 19:24:56 +0900

    221109

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+function message(){
+    alert("예매가 완료되었습니다.")
+}
+
+function showInfo(){
+    
+    var date=document.getElementById("input_date").value;
+    var concert=document.getElementById("concert").value;
+    
+    if(concert ==="선택하세요" ||  date===""){
+
+        alert("날짜와 공연을 선택해 주세요.");
+        document.querySelector('#seat_info').innerHTML="";
+
+    }
+    
+    else {
+
+
+        document.querySelector('#seat_info').innerHTML='남은좌석 : 10 \
+        <input type="button" id="join" value="예매하기" onclick="message()">' ;
+    }
+}
+
+function showLogin(){
+
+    document.querySelector('.id').innerHTML= '<input type="text" id="ID" name="ID" placeholder="아이디">';
+    document.querySelector('.pass').innerHTML= '<input type="password" id="password" name="password" placeholder="비밀번호">';
+    document.querySelector('.button').innerHTML='<input type="button" id="login" name="button" value="로그인"></button>';
+    document.querySelector('.join').innerHTML='<input type="button" id="login" name="button" \
+    value="회원가입" onclick="showLoginPage()" ></button>';
+  
+}
+
+function showLoginPage() {
+    window.open('http://127.0.0.1:5500/join.html');
+}
+
+function showSeat() {
+    window.open('http://127.0.0.1:5500/seat.html');
+}
+
+function showTicketingPage() {
+    window.open('http://127.0.0.1:5500/ticketingPage.html');
+}
+
+
+
 var Body={
             setColor : function (color){
                 // document.querySelector('body').style.color = color;
@@ -28,19 +76,6 @@ function showDate(){
     document.querySelector('#date').innerHTML=Date()
 }
 
-function showLogin(){
-
-    document.querySelector('.id').innerHTML= '<input type="text" id="ID" name="ID" placeholder="아이디">';
-    document.querySelector('.pass').innerHTML= '<input type="password" id="password" name="password" placeholder="비밀번호">';
-    document.querySelector('.button').innerHTML='<input type="button" id="login" name="button" value="로그인"></button>';
-    document.querySelector('.join').innerHTML='<input type="button" id="login" name="button" \
-    value="회원가입" onclick="showLoginPage()" ></button>';
-  
-}
-
-function showLoginPage() {
-    window.open('http://127.0.0.1:5500/join/join.html');
-}
 
 function setBtnDisable(self){
     // var btn=document.getElementById("submit_btn");
